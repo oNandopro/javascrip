b2888a29239d1bfb14e840c8098d279d8b7dc042
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-19 13:42:25 -0300

    exs 008 e 009 condições simples js no node

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velocidade de seu carro é de ${vel}`)
+if (vel > 60) {
+    console.log (`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
