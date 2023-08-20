2fd531eec4c97a8c9b53375c7f1b080e549101b8
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-20 10:33:02 -0300

    ex013 ok

--- aula012/ex012.js
+++ aula012/ex012.js
@@ -1,2 +1,12 @@
-
-var hora = 12
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`São exatamente ${hora} horas,`)
+if (hora <=7) {
+    console.log('Boa Madrugada!')
+} else if (hora <=12) {
+    console.log('Bom Dia!')
+} else if (hora <=18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
