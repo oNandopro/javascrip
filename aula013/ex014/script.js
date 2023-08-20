64ab33a2c8ddb2c8eb71a406b05f748ff0135840
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-20 16:41:08 -0300

    :hovers ok

--- aula013/ex014/script.js
+++ aula013/ex014/script.js
@@ -1,21 +1,26 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
+    var msg = window.document.getElementById('hora')
     var img = window.document.getElementById('img')
+    var boa = window.document.getElementById('msg')
     var data = new Date()
-    var hora = data.getHours()
-    //var hora = 8
-    msg.innerHTML = `Agora são exatamente ${hora} horas.`
-    if (hora >=0 && hora <12) {
+    //var hora = data.getHours()
+    var hora = 14
+    msg.innerHTML = `Agora são ${hora} horas e alguns minutos!`
+    if (hora >=6 && hora <12) {
         //BOM DIA!
         img.src = 'imagens/manha.png'
         document.body.style.backgroundColor = '#dbcd00'
+        boa.innerHTML = 'Bom dia!'
     } else if (hora >=12 && hora <=18) {
         //BOA TARDE!
         img.src = 'imagens/tarde.png'
         document.body.style.backgroundColor = '#485779'
+        boa.innerHTML = 'Boa tarde!'
     } else {
         //BOA NOITE!
         img.src = 'imagens/noite.png'
         document.body.style.backgroundColor = '#232324'
+        boa.innerHTML = 'Boa Noite!'
     }
+
 }
