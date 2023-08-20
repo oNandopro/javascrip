754c601657fd8e475b52f6a4c9dec2c1682ef22b
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-20 15:13:25 -0300

    create query ex014

--- aula013/ex014/script.js
+++ aula013/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 8
+    msg.innerHTML = `Agora são exatamente ${hora} horas.`
+    if (hora >=0 && hora <12) {
+        //BOM DIA!
+        img.src = 'imagens/manha.png'
+        document.body.style.backgroundColor = '#dbcd00'
+    } else if (hora >=12 && hora <=18) {
+        //BOA TARDE!
+        img.src = 'imagens/tarde.png'
+        document.body.style.backgroundColor = '#485779'
+    } else {
+        //BOA NOITE!
+        img.src = 'imagens/noite.png'
+        document.body.style.backgroundColor = '#232324'
+    }
+}
