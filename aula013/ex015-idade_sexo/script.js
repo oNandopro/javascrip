03805469c7b37bfa8c45da2fcdd467c2ae1d88e1
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-21 15:52:12 -0300

    creat vars imgs

--- aula013/ex015-idade_sexo/script.js
+++ aula013/ex015-idade_sexo/script.js
@@ -4,15 +4,35 @@ function verificar() {
     var anousu = document.getElementById('ano')
     var res = document.getElementById('res')
     if (anousu.value.length == 0 || Number(anousu.value) > anoatual) {
-        window.alert('[ERRO]')
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var sexusu = document.getElementsByName('radsex')
         var idade = anoatual - Number(anousu.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (sexusu[0].checked) {
             genero = 'Homem'
+            if (idade >= o && idade <=7){
+                //bebe
+            } else if (idade <21) {
+                //jovem
+            } else if (idade <50) {
+                //adulto
+            } else {
+                /idoso
+            }
         } else if (sexusu[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade <7) {
+                //bebe
+            } else if (idade <21) {
+                //jovem
+            } else if (idade <50) {
+                //adulta
+            } else {
+                //idosa
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Indentificamos que você é ${genero} de ${idade} anos!`
