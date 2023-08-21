fb1c7651f9f723cc1babfa9aab1508d48b32a724
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-21 12:54:54 -0300

    create JS erro

--- aula013/ex015-idade_sexo/script.js
+++ aula013/ex015-idade_sexo/script.js
@@ -1,1 +1,12 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var anoatu = getFullYear()
+    var ano = document.getElementById('ano')
+    var res = document.getElementById('res')
+    if (ano.value.length == 0 || ano.value > anoatu) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        window.alert('Tudo ok!')
+    }
+
+}
