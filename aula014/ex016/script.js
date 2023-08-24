3cfd9cd34e091d8b1f8bbc66a2fcec0e107f6e42
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-24 12:40:10 -0300

    create html e js ex015

--- aula014/ex016/script.js
+++ aula014/ex016/script.js
@@ -1,1 +1,9 @@
-alert('Olá')
+function Verificar() {
+    var ini = document.getElementById('fim')
+    var fim = document.getElementById('fim')
+    var pul = document.getElementById('pul')
+    var res = document.getElementById('res')
+    var but = document.getElementById('but')
+
+    
+}
