d3109aaf00cd7627a35fae2010d58eda6555ca22
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-24 09:55:20 -0300

    create class014

---
+++ aula013/repeticoes.js
@@ -0,0 +1,16 @@
+//estrutura de repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while( c <= 6)
+
+
+
+
+/* Estrutura de repetição com teste lógico no início:
+var c = 1
+while (c <=6) {
+    console.log('Tudo bem?')
+    c++
+}*/
--- repeticoes.js
+++
@@ -1,7 +0,0 @@
-console.log('tudo bem?')
-
-/*var c = 6
-while (c <=6) {
-    console.log('Tudo bem!?')
-    c++
-}*/
