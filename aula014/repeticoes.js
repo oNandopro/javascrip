c1272cfc6453a4e9b27ff6d34e3ae51b0efc3aef
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-24 11:05:03 -0300

    debug

---
+++ aula014/repeticoes.js
@@ -0,0 +1,36 @@
+console.log('Vai começar!')
+var c = 1
+for (var c=1 ; c<=4 ; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
+/*estrutura de repetição com variável e controle (inicio;teste;incr)
+
+//serve para por essa estrutura:
+
+var c = 1 //inicialização=inicio
+
+while (c <=10//teste lógico) {
+    console.log(`Passo ${c}`)
+    c++ //incremento
+}
+/* NESTA:
+for (var c=1;c<=10;c++) {
+    console.log('Passo ${c}')
+}*/
+
+
+/*estrutura de repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while( c <= 6)*/
+
+
+/* Estrutura de repetição com teste lógico no início:
+var c = 1
+while (c <=6) {
+    console.log('Tudo bem?')
+    c++
+}*/
