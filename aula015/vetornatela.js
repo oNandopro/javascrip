9d21e6720b14ab948a182e66ddd268f339c35e68
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-27 12:16:02 -0300

    vetoresnatela

--- aula015/vetornatela.js
+++ aula015/vetornatela.js
@@ -1,6 +1,25 @@
-let valores = [8, 15, 5, 13, 18, 2, 9]
-valores.sort()
-console.log(valores)
+let valores = [8, 15, 5, 13, 18, 2, 9] // os [] diz que é um array
+valores.sort() // para por os valores em ordem
+console.log(valores) // para mostrar na tela os valores dentro do array
+console.log(`Minha array possui ${valores.length} valores`) //para dizer quantos valores minha array possui.
+
+/*
+    da pra fazer assim
+obs: sempre criar um let para posição = let pos=
+
 for (let pos=0 ; pos < valores.length ; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
+    ou simplificar com for(   in   )
+
+    assim:
+    */
+
+
+for (let pos in valores ) { //para saber que posição cada valor ocupa dentro da array
+    console.log(`A posição ${pos} tem valor ${valores[pos]}`)
+}
+
+
+
+
