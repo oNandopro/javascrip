bbf5fe9364a93198d77c4a0928e453428c0a08cc
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-27 12:28:36 -0300

    Update ambiente.js

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -4,5 +4,9 @@ num.sort()
 console.log(num)
 console.log(`Nosso vetor é o ${num.length}`)
 console.log(`Nosso primeiro vetor é ${num[0]}`)
-let pos = num.indexOf(13)
-console.log(`O valor 2 está na posição ${pos}`)
+let pos = num.indexOf(1)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado`)
+} else {
+    console.log(`O valor solicitado está na posição ${pos}`)
+}
