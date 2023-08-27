7764b6ef581707e25e9b1c4a86023c0aeb57f60e
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-27 11:18:47 -0300

    Create vetornatela.js

---
+++ aula015/vetornatela.js
@@ -0,0 +1,6 @@
+let valores = [8, 15, 5, 13, 18, 2, 9]
+valores.sort()
+console.log(valores)
+for (let pos=0 ; pos < valores.length ; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
