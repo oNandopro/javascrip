0fec6c3e489f7836cd46d2aa4d9150d10b49eecb
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-31 12:37:47 -0300

    att

--- aula016ex/script.js
+++ aula016ex/script.js
@@ -1,4 +1,4 @@
-let = document.getElementById('fnum')
+let num = document.getElementById('fnum')
 let lista = document.getElementById('flista')
 let res = document.getElementById('res')
 let valores = []
@@ -12,7 +12,7 @@ function isNumero(n) {
 }
 
 function inLista(n, l) {
-    if (lista.indexOf(Number (n)) != -1) {
+    if (l.indexOf(Number (n)) != -1) {
         return true
     } else {
         return false
@@ -21,8 +21,25 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor invalido ou já existente na lista.')
+    }
+    num.value = ' ' // apaga o num do input depois de adicionar
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo temos ${tot} números cadastrados. `
     }
 }
